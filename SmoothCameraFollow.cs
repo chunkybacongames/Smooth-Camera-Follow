3028fe3609ebc5d3058efa8c7e75de5c8b505622
Sami Ahlfors <48211518+samiahlfors@users.noreply.github.com>
2021-09-27 16:00:54 +0200

    Update SmoothCameraFollow.cs

--- SmoothCameraFollow.cs
+++ SmoothCameraFollow.cs
@@ -3,13 +3,16 @@
 public class SmoothCameraFollow : MonoBehaviour
 {
     #region Variables
+    
         private Vector3 _offset;
         [SerializeField] private Transform target;
         [SerializeField] private float smoothTime;
         private Vector3 _currentVelocity = Vector3.zero;
+        
     #endregion
     
     #region Unity callbacks
+    
         private void Awake() => _offset = transform.position - target.position;
 
         private void LateUpdate()
@@ -17,5 +20,6 @@ public class SmoothCameraFollow : MonoBehaviour
             Vector3 targetPosition = target.position + _offset;
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
         }
+        
     #endregion
 }
